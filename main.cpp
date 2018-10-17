724273539848a9425c9ee93eed794a18b41c5766
Je_day <je.mouth@gmail.com>
2018-10-17 19:33:13 +0300

    funct added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
-using namespace std;
+
+
+class funct{
+  public:
+  double (*f)(double,double);
+  funct(double (*_f)(double,double)){
+      f = _f;
+  }
+
+  double operator()( double x, double  y){
+      return f(x,y);
+  }
+
+};
+
+double curve (double x,double y) {
+    return 1/(1+x*x)+1/(1+y*y);
+}
+
+
+std::pair<double,double> annealing(funct F){
+    std::pair<double,double> point = {0.0,0.0};
+
+
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    funct f(curve);
+
+
     return 0;
 }
